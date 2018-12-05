4aeded3141069e664117b5c2ed849d11be1193f0
kiditz <kiditzbastara@gmail.com>
2018-12-05 23:42:30 +0700

    add edit product by code to handle product active or not

--- cash-api/app/service/product_service.py
+++ cash-api/app/service/product_service.py
@@ -131,7 +131,7 @@ class ProductService(object):
 	
 	@Key(['id', 'category_id', 'name', 'code', 'product_type', 'sell_price', 'purchase_price'])
 	def edit_product_by_id(self, domain):
-		self.validate_product(domain)
+		
 		product = Product.query.filter_by(id=domain['id']).first()
 		if 'image' in domain:
 			document_dict = self.handle_add_image(domain['image'])['payload']
