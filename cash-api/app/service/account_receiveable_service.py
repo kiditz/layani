1e55834ace93916b4eea36888defc6578b219371
kiditz <kiditzbastara@gmail.com>
2018-12-07 01:09:52 +0700

    Fix account receiveable change status

--- cash-api/app/service/account_receiveable_service.py
+++ cash-api/app/service/account_receiveable_service.py
@@ -54,7 +54,7 @@ class AccountReceiveableService(object):
 		account_receiveable_q = AccountReceiveable.query.with_entities(*entities) \
 			.join(Order, Order.id == AccountReceiveable.order_id)\
 			.join(Customer, Customer.id == Order.customer_id)\
-			.filter(and_(Customer.id == domain['customer_id'], Order.payment_method == PaymentMethod.CREDIT, Order.status == 'I')) \
+			.filter(and_(Customer.id == domain['customer_id'], Order.payment_method == PaymentMethod.CREDIT, Order.status == 'P')) \
 			.order_by(AccountReceiveable.receiveable_date) \
 			.paginate(page, size, error_out=False)
 		account_receiveable_list = list(map(lambda x: x._asdict(), account_receiveable_q.items))
@@ -64,23 +64,23 @@ class AccountReceiveableService(object):
 	def get_account_receiveable_age(self, domain):
 		in_1_30 = AccountReceiveable.query.with_entities(func.coalesce(func.sum(AccountReceiveable.total_credit), 0).label('total_credit'))\
 		.join(Order, Order.id == AccountReceiveable.order_id)\
-		.filter(and_(Order.status == 'I', Order.payment_method == PaymentMethod.CREDIT))\
+		.filter(and_(Order.status == 'P', Order.payment_method == PaymentMethod.CREDIT))\
 		.filter(between(func.date_part('days', datetime.now() - AccountReceiveable.receiveable_date), -30, -0))\
 		.first()._asdict()
 		in_30_60 = AccountReceiveable.query.with_entities(func.coalesce(func.sum(AccountReceiveable.total_credit), 0).label('total_credit'))\
 		.join(Order, Order.id == AccountReceiveable.order_id)\
-		.filter(and_(Order.status == 'I', Order.payment_method == PaymentMethod.CREDIT))\
+		.filter(and_(Order.status == 'P', Order.payment_method == PaymentMethod.CREDIT))\
 		.filter(between(func.date_part('days', datetime.now() - AccountReceiveable.receiveable_date), -60, -30))\
 		.first()._asdict()
 		in_60_90 = AccountReceiveable.query.with_entities(func.coalesce(func.sum(AccountReceiveable.total_credit), 0).label('total_credit'))\
 		.join(Order, Order.id == AccountReceiveable.order_id)\
-		.filter(and_(Order.status == 'I', Order.payment_method == PaymentMethod.CREDIT))\
+		.filter(and_(Order.status == 'P', Order.payment_method == PaymentMethod.CREDIT))\
 		.filter(between(func.date_part('days', datetime.now() - AccountReceiveable.receiveable_date), -90, -60))\
 		.first()._asdict()
 
 		in_gt_90 = AccountReceiveable.query.with_entities(func.coalesce(func.sum(AccountReceiveable.total_credit), 0).label('total_credit'))\
 		.join(Order, Order.id == AccountReceiveable.order_id)\
-		.filter(and_(Order.status == 'I', Order.payment_method == PaymentMethod.CREDIT))\
+		.filter(and_(Order.status == 'P', Order.payment_method == PaymentMethod.CREDIT))\
 		.filter(func.date_part('days', datetime.now() - AccountReceiveable.receiveable_date) < -90)\
 		.first()._asdict()
 		return {'payload': {'1-30 Hari': in_1_30['total_credit'], '30-60 Hari': in_30_60['total_credit'], '60-90 Hari': in_60_90['total_credit'], 'Lebih Dari 90 Hari': in_gt_90['total_credit']}}
@@ -89,23 +89,23 @@ class AccountReceiveableService(object):
 	def get_account_receiveable_out_of_age(self, domain):
 		in_0_30 = AccountReceiveable.query.with_entities(func.coalesce(func.sum(AccountReceiveable.total_credit), 0).label('total_credit'))\
 		.join(Order, Order.id == AccountReceiveable.order_id)\
-		.filter(and_(Order.status == 'I', Order.payment_method == PaymentMethod.CREDIT))\
+		.filter(and_(Order.status == 'P', Order.payment_method == PaymentMethod.CREDIT))\
 		.filter(between(func.date_part('days', datetime.now() - AccountReceiveable.receiveable_date), 1, 30))\
 		.first()._asdict()
 		in_30_60 = AccountReceiveable.query.with_entities(func.coalesce(func.sum(AccountReceiveable.total_credit), 0).label('total_credit'))\
 		.join(Order, Order.id == AccountReceiveable.order_id)\
-		.filter(and_(Order.status == 'I', Order.payment_method == PaymentMethod.CREDIT))\
+		.filter(and_(Order.status == 'P', Order.payment_method == PaymentMethod.CREDIT))\
 		.filter(between(func.date_part('days', datetime.now() - AccountReceiveable.receiveable_date), 30,  60))\
 		.first()._asdict()
 		in_60_90 = AccountReceiveable.query.with_entities(func.coalesce(func.sum(AccountReceiveable.total_credit), 0).label('total_credit'))\
 		.join(Order, Order.id == AccountReceiveable.order_id)\
-		.filter(and_(Order.status == 'I', Order.payment_method == PaymentMethod.CREDIT))\
+		.filter(and_(Order.status == 'P', Order.payment_method == PaymentMethod.CREDIT))\
 		.filter(between(func.date_part('days', datetime.now() - AccountReceiveable.receiveable_date), 60, 90))\
 		.first()._asdict()
 
 		in_gt_90 = AccountReceiveable.query.with_entities(func.coalesce(func.sum(AccountReceiveable.total_credit), 0).label('total_credit'))\
 		.join(Order, Order.id == AccountReceiveable.order_id)\
-		.filter(and_(Order.status == 'I', Order.payment_method == PaymentMethod.CREDIT))\
+		.filter(and_(Order.status == 'P', Order.payment_method == PaymentMethod.CREDIT))\
 		.filter(func.date_part('days', datetime.now() - AccountReceiveable.receiveable_date) > 90)\
 		.first()._asdict()
 		return {'payload': {'0-30 Hari': in_0_30['total_credit'], '30-60 Hari': in_30_60['total_credit'], '60-90 Hari': in_60_90['total_credit'], 'Lebih Dari 90 Hari': in_gt_90['total_credit']}}
