354afa9c4dc9fdab51abd39c8485d5be9210901f
kiditz <kiditzbastara@gmail.com>
2018-12-27 05:53:56 +0700

    Add receipt print for sales

--- cash-api/app/service/order_service.py
+++ cash-api/app/service/order_service.py
@@ -234,7 +234,7 @@ class OrderService(object):
 		
 		order_cash_q = Order.query.with_entities(
 			func.coalesce(func.sum(Order.total_amount), 0.0).label('order_summary')) \
-			.filter(and_(between(Order.order_at, cashbox_summary.start_at.strftime('%Y-%m-%d %H:%M:%S'), date_now) == date_now, Order.outlet_id == outlet_id, Order.payment_method == PaymentMethod.CASH, Order.total_amount > 0.0)) \
+			.filter(and_(between(Order.order_at, cashbox_summary.start_at.strftime('%Y-%m-%d %H:%M:%S'), date_now), Order.outlet_id == outlet_id, Order.payment_method == PaymentMethod.CASH, Order.total_amount > 0.0)) \
 			.first()
 		
 		cash_in = CashboxHistory.query.with_entities(func.coalesce(func.sum(CashboxHistory.amount), 0).label('amount')) \
