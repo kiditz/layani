4a680dfeb71c197e1c03947f62cfd04e4529151a
kiditz <kiditzbastara@gmail.com>
2019-01-05 01:58:39 +0700

    Fix indent

--- cash-api/app/service/order_service.py
+++ cash-api/app/service/order_service.py
@@ -42,8 +42,7 @@ class OrderService(object):
 			datetime_now = datetime.now()
 			date_now = datetime_now.date()
 			cashbox_summary = CashboxSummary.query \
-				.filter(CashboxSummary.outlet_id == outlet_id) \
-				#.filter(cast(CashboxSummary.start_at, DATE) == date_now) \
+				.filter(CashboxSummary.outlet_id == outlet_id)\
 				.filter(CashboxSummary.status == CashboxStatus.OPEN).first()
 			if cashbox_summary is None:
 				cashbox_summary = CashboxSummary()
