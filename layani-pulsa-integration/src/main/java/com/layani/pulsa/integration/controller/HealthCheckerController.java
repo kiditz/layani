aa2b33c667fa729e13765626708d781e91327a64
kiditz <kiditzbastara@gmail.com>
2019-01-07 23:44:30 +0700

    Fix consul health

---
+++ layani-pulsa-integration/src/main/java/com/layani/pulsa/integration/controller/HealthCheckerController.java
@@ -0,0 +1,27 @@
+package com.layani.pulsa.integration.controller;
+
+import org.slerp.core.Domain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckerController {
+
+	@Autowired
+	private KafkaMessageListenerContainer<String, String> notificationContainer;
+
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+
+	@GetMapping("/health")
+	@ResponseBody
+	public Domain health(){
+		return new Domain().put("start", "success");
+	}
+}
