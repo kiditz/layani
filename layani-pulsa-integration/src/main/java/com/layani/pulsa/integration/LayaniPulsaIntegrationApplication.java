5c4a16dd6daec09db385ba48b071d743c5eee921
kiditz <kiditzbastara@gmail.com>
2019-01-09 20:09:46 +0700

    Change the config

--- layani-pulsa-integration/src/main/java/com/layani/pulsa/integration/LayaniPulsaIntegrationApplication.java
+++ layani-pulsa-integration/src/main/java/com/layani/pulsa/integration/LayaniPulsaIntegrationApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {KafkaAutoConfiguration.class})
+@RefreshScope
 @EnableDiscoveryClient
 @EnableScheduling
 @ImportResource({"classpath*:spring-context.xml"})
